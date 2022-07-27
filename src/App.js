20029008e639de924ea994d3c727f8893b6e088c
devcodypanda <83539643+devcodypanda@users.noreply.github.com>
2022-07-27 12:32:38 +0530

    lt

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './home';
 import Navbar from './navbar';
+import UniCrypt1 from './uniCrypt1';
 
 function App() {
   return (
-    <Home/>
+    <UniCrypt1/>
   );
 }
 
